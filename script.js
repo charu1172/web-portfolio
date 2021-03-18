75bd5836d9f82d27d94e1014a5c5c2c99e954357
charu1172 <72015637+charu1172@users.noreply.github.com>
2021-03-19 02:48:34 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+function togglemenu(){
+    const menutoggle = document.querySelector('.toggle')
+    const sidebar = document.querySelector('.sidebar')
+    const imgsidebar = document.querySelector('.imgsidebar')
+    const img2 = document.querySelector('.img2')
+    const img3 = document.querySelector('.img3')
+    
+    menutoggle.classList.toggle('active')
+    sidebar.classList.toggle('active')
+    imgsidebar.classList.toggle('active')
+    img2.classList.toggle('active')
+    img3.classList.toggle('active')
+
+}
